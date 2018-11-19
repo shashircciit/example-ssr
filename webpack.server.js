3d62d9e2ee259790461f304b5ce9eb1fa1958dd9
Shashi Singh <shashircciit@gmail.com>
2018-11-19 13:43:05 +0530

    Nodeexternals are added

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const merge=require('webpack-merge')
 const baseConfig=require('./webpack.base')
+const webpackNodeExternals = require('webpack-node-externals');
+
 
 const config = {
   // Inform webpack that we're building a bundle
@@ -17,6 +19,7 @@ const config = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
   },
+  externals:[webpackNodeExternals()]
 };
 
 
