b109c68df32080db6d6e746d845721a8e3fb8c4d
Shashi Singh <shashircciit@gmail.com>
2018-11-22 10:53:29 +0530

    moka page added as example for SSR

---
+++ server/src/client/pages/GouthamMoka.js
@@ -0,0 +1,170 @@
+import React, { Component } from 'react';
+import { withRouter } from "react-router-dom";
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { fetchlogin } from '../actions';
+
+const baseurl = "http://10.2.11:4069/api"
+
+class GouthamMoka extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            email: "",
+            password: "",
+            rememberMe: false
+        };
+        this.login = this.login.bind(this);
+        this.showForgotPassword = this.showForgotPassword.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+
+    login(e) {
+        e.preventDefault();
+        var self = this;
+        console.log('login called ')
+        let { email, password, rememberMe } = this.state;
+        if (email.trim() == "" || password.trim() == "") {
+            return false;
+        }
+        console.log("email is  " + email + " password is " + password);
+        try {
+            let data = { email: email, password: password };
+            this.props.fetchlogin(data)
+        } catch (err) {
+            console.log("error is " + JSON.stringify(err));
+        }
+    }
+
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+
+    showForgotPassword() {
+        swal({
+            title: "Forgot Password",
+            text: 'Please enter the registered email id. You will receive reset password link.',
+            content: "input",
+            button: {
+                text: "Submit",
+                closeModal: true,
+            },
+        })
+            .then(name => {
+                console.log('name is ...' + name);
+                if (!name) throw null;
+            })
+            .then(results => {
+                return results.json();
+            })
+            .then(json => {
+                const movie = json.results[0];
+
+                if (!movie) {
+                    return swal("No movie was found!");
+                }
+
+                const name = movie.trackName;
+                const imageURL = movie.artworkUrl100;
+
+                swal({
+                    title: "Top result:",
+                    text: name,
+                    icon: imageURL,
+                });
+            })
+            .catch(err => {
+                if (err) {
+                    swal("Oh noes!", "The AJAX request failed!", "error");
+                } else {
+                    swal.stopLoading();
+                    swal.close();
+                }
+            });
+    }
+
+
+    saveUser() {
+        console.log("done")
+        localStorage.setItem("Login Id", this.state.email)
+        localStorage.setItem("Password", this.state.password)
+        localStorage.setItem("RememberMe", this.state.rememberMe)
+    }
+
+
+
+    render() {
+        let { error } = this.state;
+       let logindata=this.props.login;
+ 
+        return (
+
+            <div className="container-scroller">
+                <div className="container-fluid page-body-wrapper full-page-wrapper">
+                    <div className="content-wrapper d-flex align-items-center auth login-full-bg">
+                        <div className="row w-100 margin-l-0">
+                            <div className="col-lg-4 mx-auto">
+                                <div className="auth-form-light text-left p-5">
+                                    <div className="login-header">
+                                        <img src="assets/images/logo.png" />
+                                        <hr />
+                                        <h4 className="font-weight-light">Admin Login</h4>
+                                         
+                                    </div>
+                                    <form className="pt-5" autoComplete="off" >
+                                        <div className="form-group">
+                                            <input className="form-control" type="text" name="email" placeholder="Enter Email" pattern="^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$" onChange={this.handleChange}/>
+                                            <i className="fa fa-user" aria-hidden="true" />
+                                        </div>
+                                        <div className="form-group">
+                                            <input className="form-control" type="password" name="password" placeholder="Enter Password" id="password" required   onChange={this.handleChange}/>
+                                            <i className="fa fa-lock" aria-hidden="true" />
+                                        </div>
+                                        <div className="mt-5">
+                                            <button className="btn btn-block btn-primary btn-lg font-weight-medium" type="submit" onClick={this.login} >Login</button>
+                                        </div>
+                                        <div className="row">
+                                            <div className="col-md-6 col-sm-6">
+                                                <div className="form-check">
+                                                    <label className="form-check-label">
+                                                        <input type="checkbox" className="form-check-input" name="rememberMe" />
+                                                        Remember Password
+                            <i className="input-helper" /></label>
+                                                </div>
+                                            </div>
+                                            <div className="col-md-6 col-sm-6">
+                                                <div className="mt-3 text-right">
+                                                    <a className="auth-link text-gray" >Forgot password?</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return { login: state.login,display:state.login };
+}
+
+
+function loadData(store) {
+    return store.dispatch(fetchlogin());
+}
+
+export default {
+    component: connect(mapStateToProps, { fetchlogin })(GouthamMoka),
+    loadData: ({ dispatch }) => dispatch(fetchlogin())
+};
